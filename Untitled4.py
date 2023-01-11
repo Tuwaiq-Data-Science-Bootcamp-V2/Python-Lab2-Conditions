0ef0b848c19894468fc4386bbafa227b543a35b5
Lamamugren <122200553+Lamamugren@users.noreply.github.com>
2023-01-11 13:54:25 +0300

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+movie = "Harry Potter"
+
+
+# In[2]:
+
+
+rating = 3
+
+
+# In[3]:
+
+
+popularity = 72.65
+
+
+# In[4]:
+
+
+print("Highly recommended")
+
+
+# In[5]:
+
+
+print("I recommended it, It is good")
+
+
+# In[6]:
+
+
+print("You should check it out!")
+
+
+# In[7]:
+
+
+print("Don't watch it, It is a waste of time")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
