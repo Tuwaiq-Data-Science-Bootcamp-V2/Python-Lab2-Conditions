d19ad3f3f3b72aeb32a05ae1228b289d6f11f921
StrKuji <105664796+StrKuji@users.noreply.github.com>
2023-01-11 13:39:48 +0300

    Lab2

---
+++ Lab2Day1.py
@@ -0,0 +1,13 @@
+Movie = 'Interstellar'
+MyRate = 3
+popularityScore = 72.65
+
+if MyRate >= 4 and popularityScore > 80:
+    print("Highly recommended")
+elif MyRate >= 3 and popularityScore > 70:
+    print("I recommended it, It is good")
+elif MyRate >= 2 and popularityScore > 60:
+    print("You should check it out!")
+elif MyRate >= 2 and popularityScore > 50:
+    print("Don't watch it, It is a waste of time")
+
