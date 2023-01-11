c5c478a99c7eb4eb749d526b9ac4818f357f320c
RaghadAlsalamah <122266444+RaghadAlsalamah@users.noreply.github.com>
2023-01-11 13:32:11 +0300

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,19 @@
+movie = x
+MR = 3
+popularity = 72.65
+
+if MR >= 4 and popularity > 80:
+    print("Highly recommended")
+
+elif MR >= 3 and popularity > 70:
+    print("I recommended it, It is good")
+
+elif MR <= 2 and popularity > 60: 
+    print("You should check it out!")
+
+elif MR <= 2 and popularity > 50: 
+    print("Don't watch it, It is a waste of time")
+
+   
+
+
